528c7060a72d2ea79b03d46c8c5b1398135a36f2
ishuparmar529 <ishuparmar529@gmail.com>
2025-04-08 12:13:12 +0530

    remove previous files

--- src/constants.ts
+++
@@ -1,9 +0,0 @@
-// Existing constants
-export const BASE_URL =
-  import.meta.env.VITE_API_URL ||
-  "http://127.0.0.1:8000";
-
-// Application settings with defaults
-export const DEFAULT_APP_SETTINGS = {
-  maxParallelStepAnalysis: 1,
-};
